6e57aec7c9dc90bcc15ce805c541a253a2d22772
soultatisl779480 <91250797+soultatisl779480@users.noreply.github.com>
2022-04-08 15:54:13 +0800

    Update thruster_test.ino

--- thruster_code/thruster_test.ino
+++ thruster_code/thruster_test.ino
@@ -1,3 +1,4 @@
+#define Serial Serial1
 #include <Servo.h>
 #include "Adafruit_BNO055.h"
 
@@ -7,22 +8,19 @@ Servo motBL;
 Servo motFL;
 Servo motR;
 Servo motL;
-Servo claw_rotate;
-Servo claw_grab;  
+Servo claw_servo;
 
 void setup() {
-  // Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial.begin(9600);
   
-  claw_rotate.attach(13);
-  claw_grab.attach(14);
+  claw_servo.attach(12);
   
   motFR.attach(A5, 1000, 2000);  
   motBR.attach(A2, 1000, 2000);
   motBL.attach(A4, 1000, 2000);
   motFL.attach(A3, 1000, 2000);
   motR.attach(10, 1000, 2000);
-  motL.attach(9, 1000, 2000);
+  motL.attach(5, 1000, 2000);
   
   motFR.writeMicroseconds(1500);
   motBR.writeMicroseconds(1500);
@@ -32,25 +30,30 @@ void setup() {
   motL.writeMicroseconds(1500);
   
   delayMicroseconds(5000);
-  while (!Serial1);
-  // while (!Serial);
-  // Serial.println("ready");
+  while (!Serial);
 }
 
 void loop() {
-  if(Serial1.available() >= 4){
-    byte header = Serial1.read();
+  if(Serial.available() >= 4){
+    byte header = Serial.read();
     if (header == 1){
-      uint8_t motor = Serial1.read();
-      uint8_t m = Serial1.read();   
-      byte footer = Serial1.read();
-
-      // Serial.println(motor);
-      // Serial.println(m);
+      uint8_t motor = Serial.read();
+      uint8_t m = Serial.read();   
+      byte footer = Serial.read();
 
       if (footer != 255) return;
       
-      /*if (m == 0){
+      if (m == 0){
+        m = 1500;
+      }
+      /*else if (m < 0){ 
+        m = map(m, -127, -1, 1000, 1499);
+      }
+      else if (m > 0){
+        m = map(m, 1, 127, 1501, 2000);
+      }
+      
+      if (m == 0){
         m = 1500;
       }
       else if (m < 0){ 
@@ -58,27 +61,32 @@ void loop() {
       }
       else if (m > 0){
         m = map(m, 1, 127, 1501, 2000);
-      }*/
-      
+      }   */
       if (motor == 2){
         motFR.writeMicroseconds(m * 10);
+        motFL.writeMicroseconds(m * 10);
+        Serial.print("Motor FR+FL");
       } 
       else if (motor == 3){
-        motFL.writeMicroseconds(m * 10);
-      } 
-      else if (motor == 4){
         motBR.writeMicroseconds(m * 10);
+        motBL.writeMicroseconds(m * 10);
+        Serial.print("Motor BR+BL");
+      } 
+      /*else if (motor == 4){
+        motBR.writeMicroseconds(m * 10);
+        Serial.print("Motor BR");
       } 
       else if (motor == 5){
         motBL.writeMicroseconds(m * 10);
-      } 
+        Serial.print("Motor BL");
+      } */
       else if (motor == 6){
         motR.writeMicroseconds(m * 10);
-        
+        Serial.print("Motor R");
       } 
       else if (motor == 7){
         motL.writeMicroseconds(m * 10);
-        
+        Serial.print("Motor L");
       }
       else if (motor == 13){
         if (m == 127){
@@ -86,18 +94,21 @@ void loop() {
           motFL.writeMicroseconds(1700);
           motBR.writeMicroseconds(1700);
           motBL.writeMicroseconds(1700);
+          Serial.print("Top Motors Up");
         }
         else if (m == 254){
           motFR.writeMicroseconds(1300);
           motFL.writeMicroseconds(1300);
           motBR.writeMicroseconds(1300);
           motBL.writeMicroseconds(1300);
+          Serial.print("Top Motors Down");
         }
         else if (m == 0){
           motFR.writeMicroseconds(1500);
           motFL.writeMicroseconds(1500);
           motBR.writeMicroseconds(1500);
           motBL.writeMicroseconds(1500);
+          Serial.print("Top Motors Off");
         }
       }
       else if (motor == 14){
@@ -107,6 +118,17 @@ void loop() {
           motBL.writeMicroseconds(1500);
           motR.writeMicroseconds(1500);
           motL.writeMicroseconds(1500);
+          Serial.print("Motors Kill");
+      }
+      else if (motor == 9){
+        if (m == 11){
+          claw_servo.write(0);
+          Serial.print("Claw Close");
+        }
+          if (m == 12){
+          claw_servo.write(120);
+          Serial.print("Claw Open");
+        }
       }
     }
   }
