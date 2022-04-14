126d7e210dd1bcd8afd19211d09f738a45c56cd2
chickwensrule <80529810+ChickwensRule@users.noreply.github.com>
2022-04-14 16:08:35 +0800

    remove cam

--- gui/active.py
+++ gui/active.py
@@ -2,7 +2,6 @@ from PyQt5.QtWidgets import QTabWidget
 from PyQt5.QtCore import Qt
 
 from gui.tabs.home import HomeTab
-from gui.tabs.camera import Camera 
 from gui.tabs.console import ConsoleTab
 
 import yaml
@@ -13,12 +12,10 @@ class ActiveTab(QTabWidget):
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
         
         self.home_tab = HomeTab(parent)
-        self.cam_tab = Camera(settings['camera-port'])
         self.console_tab = ConsoleTab()
 
 
         self.addTab(self.home_tab, 'Menu')
-        self.addTab(self.cam_tab, 'Camera')
         self.addTab(self.console_tab, 'Console')
 
         self.setTabPosition(QTabWidget.West)
