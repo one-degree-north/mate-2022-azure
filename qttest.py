5e3480e64b4aae03865665765db1304c15515198
chickwensrule <80529810+ChickwensRule@users.noreply.github.com>
2022-04-26 16:33:27 +0800

    test

--- qttest.py
+++ qttest.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel
+from PyQt5.QtGui import QFont
 
 import sys
 
@@ -6,9 +7,25 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        # self.container = QWidget()
+
+        # self.container.layout = QVBoxLayout()
+
         self.button = QPushButton('ok')
+        self.button.setStyleSheet('font: bold 50px')
+    #     self.button.clicked.connect()
+
+    #     self.ok_text = QLabel('')
+
+    #     self.container.layout.addWidget(self.button)
+    #     self.container.layout.addWidget(self.counter)
+
+    #     self.container.setLayout(self.container.layout)
 
         self.setCentralWidget(self.button)
+
+    # def update_counter(self):
+    #     pass
 
 
 app = QApplication([])
